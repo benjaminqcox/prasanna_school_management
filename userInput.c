9ceeeaa6cf374b2059dbd3ae4ef55f6af3d5645c
benjaminqcox <benjaminqcox@gmail.com>
2023-09-11 11:55:44 +0100

    Added int range user input and changed do while to while loops (to always initialize values)

--- userInput.c
+++ userInput.c
@@ -12,7 +12,8 @@ char * getStringInput()
     size_t last = 0;
     char *buf = NULL;
 
-    do {
+    do
+    {
         size += BUFSIZ; /* BUFSIZ is defined as "the optimal read size for this platform" */
         buf = realloc(buf, size); /* realloc(NULL,n) is the same as malloc(n) */            
         /* Actually do the read. Note that fgets puts a terminal '\0' on the
@@ -31,12 +32,27 @@ char * getStringInput()
 
 int getIntInput()
 {
-    char * endPtr;
-    long chosenVal = strtol(getStringInput(), &endPtr, 10);
-    return (int)chosenVal;
+    char * endPtr = NULL;
+    char * strVal = getStringInput();
+    int chosenVal = (int)strtol(strVal, &endPtr, 10);
+    // while chosenVal == 0 and the user didn't input 0, keep looping
+    // strtol fills invalid inputs with 0
+    while (chosenVal == 0 && strncmp(strVal, "0", 1) != 0)
+    {
+        endPtr = NULL;
+        strVal = getStringInput();
+        chosenVal = (int)strtol(strVal, &endPtr, 10);
+    }
+
+    return chosenVal;
 }
 
 int getIntInputInRange(int low, int high)
 {
-    return 0;
+    int chosenVal = getIntInput();
+    while (chosenVal < low || chosenVal > high)
+    {
+        chosenVal = getIntInput();
+    }
+    return chosenVal;
 }
